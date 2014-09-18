c38deeeb6ebc24ba9bf96fab482c13fe1ebdbfa2
ygol <admin@ygol.com>
2014-09-18 13:11:48 +0200

    advanced views

--- openacademy/models.py
+++ openacademy/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from openerp import models, fields, api
+from datetime import timedelta
+from openerp import models, fields, api, exceptions
 
 # class openacademy(models.Model):
 #     _name = 'openacademy.openacademy'
@@ -18,6 +19,29 @@ class Course(models.Model):
     session_ids = fields.One2many(
         'openacademy.session', 'course_id', string="Sessions")
 
+    @api.one
+    def copy(self, default=None):
+        default = dict(default or {})
+
+        copied_count = self.search_count(
+            [('name', '=like', u"Copy of {}%".format(self.name))])
+        if not copied_count:
+            new_name = u"Copy of {}".format(self.name)
+        else:
+            new_name = u"Copy of {} ({})".format(self.name, copied_count)
+
+        default['name'] = new_name
+        return super(Course, self).copy(default)
+
+    _sql_constraints = [
+        ('name_description_check',
+        'CHECK(name != description)',
+        "The title of the course should not be the description"),
+        ('name_unique',
+        'UNIQUE(name)',
+        "The course title must be unique"),
+    ]
+
 class Session(models.Model):
     _name = 'openacademy.session'
 
@@ -26,6 +50,7 @@ class Session(models.Model):
     duration = fields.Float(digits=(6,2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
     active = fields.Boolean(default=True)
+    color = fields.Integer()
 
     instructor_id = fields.Many2one('res.partner', string="Instructor",
                                     domain=['|', ('instructor', '=', True),('category_id.name', 'ilike', "Teacher")])
@@ -35,17 +60,31 @@ class Session(models.Model):
 
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
     total_qty = fields.Integer(string=None, compute='_total_qty')
+    end_date = fields.Date(string="End Date", store=True,
+                           compute='_get_end_date', inverse='_set_end_date')
+
+    hours = fields.Float(string="Duration in hours",
+                         compute='_get_hours', inverse='_set_hours')
+
+    attendees_count = fields.Integer(
+        string="Attendees count", compute='_get_attendees_count', store=True)
+
+    @api.one
+    @api.depends('attendee_ids')
+    def _get_attendees_count(self):
+        self.attendees_count = len(self.attendee_ids)
+
 
     @api.one
     @api.depends('seats', 'attendee_ids')
     def _taken_seats(self):
-        print "before test"
+
         # import ipdp; ipdb.set_trace()
         if not self.seats:
-            print "first if"
+
             self.taken_seats = 0.0
         else:
-            print "second cond else"
+
             self.taken_seats = 100.0 * len(self.attendee_ids) / self.seats
 
     def _total_qty(self):
@@ -73,4 +112,42 @@ class Session(models.Model):
                     'message': "Increase seats or remove excess attendees",
                 },
             }
+    @api.one
+    @api.constrains('instructor_id', 'attendee_ids')
+    def _check_instructor_not_in_attendees(self):
+        if self.instructor_id and self.instructor_id in self.attendee_ids:
+            raise exceptions.ValidationError("A session's instructor can't be an attendee")
+
+    @api.one
+    @api.depends('start_date', 'duration')
+    def _get_end_date(self):
+        if not (self.start_date and self.duration):
+            self.end_date = self.start_date
+            return
+
+        # Add duration to start_date, but: Monday + 5 days = Saturday, so
+        # subtract one second to get on Friday instead
+        start = fields.Datetime.from_string(self.start_date)
+        duration = timedelta(days=self.duration, seconds=-1)
+        self.end_date = start + duration
+
+    @api.one
+    def _set_end_date(self):
+        if not (self.start_date and self.end_date):
+            return
+
+        # Compute the difference between dates, but: Friday - Monday = 4 days,
+        # so add one day to get 5 days instead
+        start_date = fields.Datetime.from_string(self.start_date)
+        end_date = fields.Datetime.from_string(self.end_date)
+        self.duration = (end_date - start_date).days + 1
+
+    @api.one
+    @api.depends('duration')
+    def _get_hours(self):
+        self.hours = self.duration * 24
+
+    @api.one
+    def _set_hours(self):
+        self.duration = self.hours / 24
 

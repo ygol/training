2afcede97a3bf08886794f36edb3510eb62a6790
ygol <admin@ygol.com>
2014-09-19 11:56:05 +0200

    wizard

--- openacademy/__openerp__.py
+++ openacademy/__openerp__.py
@@ -25,7 +25,8 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/security.xml',
+        'security/ir.model.access.csv',
         'templates.xml',
         'views/course.xml',
         'views/session.xml',
